3223fa4bf79c65d29b5e65d4c161c07d277ee856
Michael Talmadge <michael.talmadge314159@gmail.com>
2017-01-15 13:31:55 -0800

    Merged from Apple Juice branch

---
+++ MainWindowController.swift
@@ -0,0 +1,18 @@
+//
+//  MainWindowController.swift
+//  RGBWell
+//
+//  Created by Michael Talmadge on 1/15/17.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController  {
+    
+    override var windowNibName: String? {
+        return "MainWindowController"
+        
+    }
+    
+}
