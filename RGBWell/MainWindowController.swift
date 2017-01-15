41d55bd0de71c647847b8be88939dbe83c4e7823
Michael Talmadge <michael.talmadge314159@gmail.com>
2017-01-15 12:42:25 -0800

    Removed Xcode generated main window.
    created new MainWindowController - both
    .swift and .xib.
    
    removed visible at launch check
    
    add ‘single window delegate app snippet to:
    	1) var mainWindowController
    	2) show itself
    	3) keep reference to window in app delegate

---
+++ RGBWell/MainWindowController.swift
@@ -0,0 +1,24 @@
+//
+//  MainWindowController.swift
+//  RGBWell
+//
+//  Created by Michael Talmadge on 1/15/17.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+    
+    
+    override var windowNibName: String?  {
+        return "MainWindowController"
+    }
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
