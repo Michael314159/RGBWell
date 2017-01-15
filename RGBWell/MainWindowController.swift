53ee949d83770d46f8b73f759f82a7477126b96a
Michael Talmadge <michael.talmadge314159@gmail.com>
2017-01-15 13:23:13 -0800

    Added bindings and actions to sliders, labels, color well and connected defaults values to controls.

--- RGBWell/MainWindowController.swift
+++ RGBWell/MainWindowController.swift
@@ -11,6 +11,45 @@ import Cocoa
 class MainWindowController: NSWindowController {
     
     
+    var redLabel = 0.5
+    var greenLabel = 0.5
+    var blueLabel = 0.5
+    
+    let alpha = 1.0 //opacity value neede by NSColor()
+    
+    
+    @IBOutlet weak var colorWell: NSColorWell!
+    
+    
+    
+    func updateColor() {
+        let newColor = NSColor(calibratedRed: CGFloat(redLabel), green: CGFloat(greenLabel), blue: CGFloat(blueLabel), alpha: CGFloat(alpha))
+        colorWell.color = newColor
+        
+    }
+    
+    
+    
+    
+    
+    @IBAction func adjustRed(_ sender: NSSlider) {
+        redLabel = sender.doubleValue
+        updateColor()
+        
+    }
+    
+    
+    @IBAction func adjustGreen(_ sender: NSSlider) {
+        greenLabel = sender.doubleValue
+        updateColor()
+    }
+    
+    
+    @IBAction func adjustBlue(_ sender: NSSlider) {
+        blueLabel = sender.doubleValue
+        updateColor()
+    }
+    
     override var windowNibName: String?  {
         return "MainWindowController"
     }
@@ -19,6 +58,13 @@ class MainWindowController: NSWindowController {
         super.windowDidLoad()
 
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        
+        
+        //update colorwell to label numbers...
+        updateColor()
+        
+        
+        
     }
     
 }
